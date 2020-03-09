e1a429d8ae22d8cbb0485193e94f707594c95ae5
kdynamita <tang.kenley@hotmail.com>
2020-03-09 13:57:30 -0400

    SceneLoader + StartMenu + TextMeshPro

---
+++ VvV_Project/Assets/_VvV/Scripts/SceneManagement/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour {
+    //Call Toolbox.GetInstance().GetScene().LoadScene(*Add scene name*); in other scripts
+
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
