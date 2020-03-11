6c0fa00d6149ce5efb703374b342a9af2b67ef24
omgitstri <omgitstri@hotmail.com>
2020-03-10 21:27:39 -0400

    push level design tool, custom triggers

---
+++ VvV_Project/Assets/_Tri/IntepolateTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntepolateTest : MonoBehaviour
+{
+    public Transform start, middle, stop;
+    public float a;
+    Vector3 radius;
+
+    [ContextMenu(nameof(start))]
+    private void Start()
+    {
+        radius = (Random.insideUnitSphere * 2);
+    }
+
+    void Update()
+    {
+        Vector3 lerpA;
+        lerpA = Vector3.Lerp(start.position, middle.position + radius, a);
+        transform.position = Vector3.Lerp(lerpA, stop.position, a);
+    }
+
+    private void OnValidate()
+    {
+        Vector3 lerpA;
+        lerpA = Vector3.Lerp(start.position, middle.position + radius, a);
+        transform.position = Vector3.Lerp(lerpA, stop.position, a);
+    }
+}
