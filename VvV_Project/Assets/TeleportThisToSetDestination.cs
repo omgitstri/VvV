106cb06123b1bd76610c5ad1757d97f2c9afd733
zigie_inc <zigie_inc@hotmail.com>
2020-04-22 21:48:34 -0400

    I got the teleport working! I iz less n00bsCake!

---
+++ VvV_Project/Assets/TeleportThisToSetDestination.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///      Teleport object with script to attached to to set destinatio
+/// </summary>
+
+public class TeleportThisToSetDestination : MonoBehaviour
+{
+
+	public void TeleportHere(Transform destination)
+	{
+		transform.position = destination.position;
+
+		//Transform thisTarget.position = destination.position;
+	}
+
+	
+}
