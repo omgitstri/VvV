18666f790166eec95685866aca396628a8e0d30c
trinplaytika <minhn@playtika.com>
2020-04-26 23:37:00 -0400

    add victory condition

---
+++ VvV_Project/Assets/_VvV/Scripts/EndGameScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameScript : MonoBehaviour
+{
+    [SerializeField] LayerMask layer = ~0;
+
+    int triggeredCount = 2;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (layer == (layer | 1 << other.gameObject.layer))
+        {
+            triggeredCount -= 1;
+        }
+        if (triggeredCount <= 0)
+        {
+            Toolbox.GetInstance.GetScene().LoadScene("Victory");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (layer == (layer | 1 << other.gameObject.layer))
+            triggeredCount++;
+    }
+}
