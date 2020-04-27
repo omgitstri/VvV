33d56ddd151274c549821f066ae8f531640fb635
kdynamita <tang.kenley@hotmail.com>
2020-04-26 23:50:33 -0400

    Added Victory Scene & fixed Player Prefab

---
+++ VvV_Project/Assets/_VvV/Scripts/SceneLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoad : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeScene() {
+        Toolbox.GetInstance.GetScene().LoadScene(sceneName);
+    }
+}
