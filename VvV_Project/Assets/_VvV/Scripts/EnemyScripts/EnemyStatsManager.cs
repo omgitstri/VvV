c0a6839a6067a8cc5d7029f57f4a61941ec430fb
kdynamita <tang.kenley@hotmail.com>
2020-05-03 20:32:13 -0400

    Modified PowerUp() in EnemyStatsManager

--- VvV_Project/Assets/_VvV/Scripts/EnemyScripts/EnemyStatsManager.cs
+++ VvV_Project/Assets/_VvV/Scripts/EnemyScripts/EnemyStatsManager.cs
@@ -8,20 +8,20 @@ public class EnemyStatsManager : MonoBehaviour {
 
     /// Handlers
     private EnemyMovementState enemyMovement;
-    [SerializeField] private float pwrUpMulti = 0.15f;
-    [SerializeField] private float maxStat = 3f; 
+    [SerializeField] private float pwrUpMulti = 0.20f;
+    [SerializeField] private int maxStat = 10; 
 
     public void PowerUp(EnemyStats eStats) {
         // Increase RegenSpeed, AttackSpeed, MoveSpeed, & AggroRange according to Regen count
         eStats.regen += 1; // int regen
 
-        if (eStats.moveSpd >= maxStat) {
-            eStats.moveSpd = maxStat;
+        if (eStats.regen >= maxStat) {
+            eStats.regen = maxStat;
             eStats.isMaxed = true;
         }
 
         if (!eStats.isMaxed) {
-            //eStats.regSpd += pwrUpMulti; // Regen speed
+            eStats.minRegenSpeed += pwrUpMulti; // Regen speed
             //eStats.atkSpd -= pwrUpMulti; // Attack speed
             eStats.moveSpd += pwrUpMulti; // Move speed
             //eStats.aggroRng += pwrUpMulti; // Aggro range
