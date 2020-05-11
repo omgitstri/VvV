392c81666b66e99efa45e4a91ae24e51cd485569
trinplaytika <minhn@playtika.com>
2020-05-11 13:10:51 -0400

    added monobehaviour callbacks script

---
+++ VvV_Project/Assets/_VvV/Scripts/Tool/MonobehaviourCallbacks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonobehaviourCallbacks : MonoBehaviour
+{
+    public UnityEvent onStart;
+    public UnityEvent onEnable;
+    public UnityEvent onDisable;
+    public UnityEvent onDestroy;
+
+    void Start()
+    {
+        onStart.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        onEnable.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        onDisable.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        onDestroy.Invoke();
+    }
+}
