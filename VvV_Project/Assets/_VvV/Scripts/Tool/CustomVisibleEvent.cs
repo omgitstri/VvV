08163eaaeee2c26a195c2238ebdd1c9678a7b5a3
trinplaytika <minhn@playtika.com>
2020-05-11 13:06:29 -0400

    Add Visible Events

---
+++ VvV_Project/Assets/_VvV/Scripts/Tool/CustomVisibleEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomVisibleEvent : MonoBehaviour
+{
+    public UnityEvent onBecomeVisible;
+    public UnityEvent onBecomeInvisible;
+
+    private void OnBecameVisible()
+    {
+        onBecomeVisible.Invoke();
+    }
+
+    private void OnBecameInvisible()
+    {
+        onBecomeInvisible.Invoke();
+    }
+}
