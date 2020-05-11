8302630602409d6dce2d3d9ba869a9229d7c0a94
zigie_inc <zigie_inc@hotmail.com>
2020-05-11 02:16:45 -0400

    - First draft of the final level - Missing trigger scripts - Missing updated textures - Fixed Player's health UI bar

---
+++ VvV_Project/Assets/_VvV/Scripts/Trigger_DisableMesh.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///      Disable Mesh of product on Start
+/// </summary>
+
+public class Trigger_DisableMesh : MonoBehaviour
+{
+    // Variables
+    public MeshRenderer rend;
+    public bool disableMesh = true;
+
+
+    void Start()
+    {
+        if (disableMesh == true)
+        {
+            rend = GetComponent<MeshRenderer>();
+            rend.enabled = false;
+        }
+
+
+    }
+
+}
