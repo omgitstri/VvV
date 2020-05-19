8daa9d9dfd6c4cd25fedce75e5b875008bb95186
kdynamita <tang.kenley@hotmail.com>
2020-05-18 23:21:58 -0400

    Fixed ADS bobbing bug + Needs X axis movement fix

--- VvV_Project/Assets/HeadBobbing.cs
+++ VvV_Project/Assets/HeadBobbing.cs
@@ -13,6 +13,10 @@ public class HeadBobbing : MonoBehaviour
     [Space]
     [SerializeField]private Vector3 idleSpeed;
     [SerializeField]private Vector3 movementSpeed;
+    [SerializeField] private float walkMultiplier;
+    [SerializeField] private float runMultiplier;
+    [SerializeField] private float walkBob;
+    [SerializeField] private float runBob;
     private Vector3 targetBobPos;
 
     private void Start() {
@@ -24,30 +28,38 @@ public class HeadBobbing : MonoBehaviour
     }
 
 
-    /*
+
     void Update() {
 
-        
-        if (!player.isMoving) {
-            HeadBob(idleCounter, idleSpeed.x, idleSpeed.y);
-            idleCounter += Time.deltaTime;
+        if (player.aimer.enabled) {
+
+            // Idle
+            if (!player.isMoving) {
+                HeadBob(idleCounter, idleSpeed.x, idleSpeed.y);
+                idleCounter += Time.deltaTime;
+            }
+
+            // Run
+            else if (player.isMoving && player.isRun) {
+                HeadBob(movementCounter, movementSpeed.x * runMultiplier, movementSpeed.y * runMultiplier);
+                movementCounter += Time.deltaTime * runBob;
+            }
+
+
+            // walk
+            else {
+                HeadBob(movementCounter, movementSpeed.x * walkMultiplier, movementSpeed.y * walkMultiplier);
+                movementCounter += (Time.deltaTime * walkBob);
+            }
+
+            weaponHolder.localPosition = Vector3.Lerp(weaponHolder.localPosition, targetBobPos, Time.deltaTime * 5f);
         }
 
-        else if (player.isMoving && player.isRun) {
-            HeadBob(movementCounter, movementSpeed.x * 2, movementSpeed.y * 2);
-            movementCounter += Time.deltaTime * 2.5f;
-        }
-
-        else {
-            HeadBob(movementCounter, movementSpeed.x, movementSpeed.y);
-            movementCounter += Time.deltaTime * 2f;
-        }
-
-        weaponHolder.localPosition = Vector3.Lerp(weaponHolder.localPosition, targetBobPos, Time.deltaTime * 8f);
-
-    } */
+    }
 
     public void HeadBob(float v_z, float x_intensity, float y_intensity) {
-        targetBobPos = weaponHolderOrigin + new Vector3(Mathf.Cos(v_z * 2) * x_intensity, Mathf.Sin(v_z * 2) * y_intensity,  0f);
+        targetBobPos = weaponHolderOrigin + new Vector3(Mathf.Cos(v_z * 2) * x_intensity, 
+                                                        Mathf.Sin(v_z * 2) * y_intensity, 
+                                                        weaponHolderOrigin.z);
     }
 }
