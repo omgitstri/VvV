94d1726247de2672860cc1cd842ca3f83ae08ffb
kdynamita <tang.kenley@hotmail.com>
2020-05-19 00:06:36 -0400

    Headbobbing fixed + ADS bug fixed

--- VvV_Project/Assets/HeadBobbing.cs
+++ VvV_Project/Assets/HeadBobbing.cs
@@ -21,10 +21,11 @@ public class HeadBobbing : MonoBehaviour
 
     private void Start() {
         weaponHolderOrigin = weaponHolder.localPosition;
+        //weaponHolder.localPosition = weaponHolderOrigin;
         player = GetComponent<PlayerController>();
 
-        idleSpeed = new Vector3(idleCounter, idleSpeed.y, idleSpeed.z);
-        movementSpeed = new Vector3(movementCounter, movementSpeed.y, movementSpeed.z);
+        idleSpeed = new Vector3(idleSpeed.x, idleSpeed.y, idleSpeed.z);
+        movementSpeed = new Vector3(movementSpeed.x, movementSpeed.y, movementSpeed.z);
     }
 
 
@@ -42,7 +43,7 @@ public class HeadBobbing : MonoBehaviour
             // Run
             else if (player.isMoving && player.isRun) {
                 HeadBob(movementCounter, movementSpeed.x * runMultiplier, movementSpeed.y * runMultiplier);
-                movementCounter += Time.deltaTime * runBob;
+                movementCounter += (Time.deltaTime * runBob);
             }
 
 
@@ -55,10 +56,15 @@ public class HeadBobbing : MonoBehaviour
             weaponHolder.localPosition = Vector3.Lerp(weaponHolder.localPosition, targetBobPos, Time.deltaTime * 5f);
         }
 
+        if (!player.aimer.enabled) {
+            weaponHolder.localPosition = weaponHolderOrigin;
+        }
+
     }
 
     public void HeadBob(float v_z, float x_intensity, float y_intensity) {
-        targetBobPos = weaponHolderOrigin + new Vector3(Mathf.Cos(v_z * 2) * x_intensity, 
+        
+        targetBobPos = weaponHolderOrigin + new Vector3(Mathf.Cos(v_z) * x_intensity, 
                                                         Mathf.Sin(v_z * 2) * y_intensity, 
                                                         weaponHolderOrigin.z);
     }
