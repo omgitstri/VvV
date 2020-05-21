355df50c4ff9efc7257a270f4bd0e8360587dbf8
trinplaytika <minhn@playtika.com>
2020-05-20 23:59:45 -0400

    implemented ammo crates

---
+++ VvV_Project/Assets/_VvV/Scripts/Action/ResupplyAmmo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResupplyAmmo : MonoBehaviour
+{
+    public void Resupply()
+    {
+        Entity_Tracker.Instance.playerGun.carryBulletCount = Entity_Tracker.Instance.playerGun.maxBulletCount;
+        Entity_Tracker.Instance.playerGun.currentBulletCount = Entity_Tracker.Instance.playerGun.reloadBulletCount;
+    }
+}
