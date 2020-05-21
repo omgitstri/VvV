6e82026529d08cd3291124cd97686493abe9c27d
trinplaytika <minhn@playtika.com>
2020-05-21 19:32:31 -0400

    link level 1 and 4

---
+++ VvV_Project/Assets/_VvV/Scripts/Tool/CustomTriggerEvent2.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class CustomTriggerEvent2 : MonoBehaviour
+{
+    public bool ignoreRaycastLayer = true;
+    public LayerMask layerMask = ~0;
+
+    [HideInInspector] public bool ToggleOnTriggerEnter = false;
+    [HideInInspector] public bool ToggleOnTriggerExit = false;
+    [HideInInspector] public bool ToggleOnTriggerStay = false;
+
+    [Space]
+
+    [HideInInspector] public UnityEvent _OnTriggerEnter;
+    [HideInInspector] public UnityEvent _OnTriggerExit;
+    [HideInInspector] public UnityEvent _OnTriggerStay;
+
+    private List<Transform> triggeredObjects = new List<Transform>();
+    private List<Collider> triggeredColliders = new List<Collider>();
+    private float intervals = 0.5f;
+
+    //private void OnValidate()
+    //{
+    //    GetComponent<Collider>().isTrigger = true;
+    //    if (ignoreRaycastLayer == true)
+    //    {
+    //        gameObject.layer = (1 << 1);
+    //    }
+    //}
+
+    private void Update()
+    {
+        CheckCollidersInTrigger();
+    }
+
+    private void CheckCollidersInTrigger()
+    {
+        intervals -= Time.deltaTime;
+        if (intervals <= 0)
+        {
+            triggeredColliders.Clear();
+            triggeredColliders.AddRange(Physics.OverlapBox(GetComponent<Collider>().bounds.center, GetComponent<Collider>().bounds.extents, transform.rotation, layerMask));
+
+            if (triggeredColliders.Count <= 0)
+            {
+                triggeredObjects.Clear();
+                foreach (var item in triggeredColliders)
+                {
+                    triggeredObjects.Add(item.transform);
+                }
+                _OnTriggerExit.Invoke();
+
+            }
+            else if (triggeredObjects.Count > 0)
+            {
+                _OnTriggerEnter.Invoke();
+            }
+            intervals = 1f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (ToggleOnTriggerEnter && layerMask == (layerMask | 1 << other.gameObject.layer))
+        {
+            if (!triggeredObjects.Contains(other.transform))
+            {
+                triggeredObjects.Add(other.transform);
+            }
+
+            _OnTriggerEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (ToggleOnTriggerExit/* && layerMask == (layerMask | 1 << other.gameObject.layer)*/)
+        {
+            if (triggeredObjects.Contains(other.transform))
+            {
+                triggeredObjects.Remove(other.transform);
+            }
+
+            if (/*triggeredObjects.Count <= 0 && */triggeredColliders.Count <= 0)
+            {
+                _OnTriggerExit.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (ToggleOnTriggerStay && layerMask.value == 1 << other.gameObject.layer)
+        {
+            _OnTriggerStay.Invoke();
+        }
+    }
+}
---
+++ VvV_Project/Assets/_VvV/Scripts/Tool/Editor/CustomTriggerEvent2Editor.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomTriggerEvent2))]
+public class CustomTriggerEvent2Editor : Editor
+{
+    CustomTriggerEvent2 editObject = null;
+    string enter = "Activate Enter";
+    string exit = "Activate Exit";
+    string stay = "Activate Stay";
+
+    private void OnEnable()
+    {
+        editObject = (CustomTriggerEvent2)target;
+
+
+        if (!editObject.ToggleOnTriggerEnter)
+        {
+            enter = "Activate Enter";
+        }
+        else
+        {
+            enter = "Deactivate Enter";
+        }
+        if (!editObject.ToggleOnTriggerExit)
+        {
+            exit = "Activate Exit";
+        }
+        else
+        {
+            exit = "Deactivate Exit";
+        }
+        if (!editObject.ToggleOnTriggerStay)
+        {
+            stay = "Activate Stay";
+        }
+        else
+        {
+            stay = "Deactivate Stay";
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal(GUILayout.Width(Screen.width * 2 / 3));
+        {
+            if (GUILayout.Button(enter))
+            {
+                if (editObject.ToggleOnTriggerEnter)
+                {
+                    editObject.ToggleOnTriggerEnter = false;
+                    enter = "Activate Enter";
+                }
+                else
+                {
+                    editObject.ToggleOnTriggerEnter = true;
+                    enter = "Deactivate Enter";
+                }
+            }
+
+            if (GUILayout.Button(exit))
+            {
+                if (editObject.ToggleOnTriggerExit)
+                {
+                    editObject.ToggleOnTriggerExit = false;
+                    exit = "Activate Exit";
+                }
+                else
+                {
+                    editObject.ToggleOnTriggerExit = true;
+                    exit = "Deactivate Exit";
+                }
+            }
+
+            if (GUILayout.Button(stay))
+            {
+                if (editObject.ToggleOnTriggerStay)
+                {
+                    editObject.ToggleOnTriggerStay = false;
+                    stay = "Activate Stay";
+                }
+                else
+                {
+                    editObject.ToggleOnTriggerStay = true;
+                    stay = "Deactivate Stay";
+                }
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+
+
+
+        if (editObject.ToggleOnTriggerEnter)
+        {
+            //EditorGUILayout.PropertyField(editObject);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(editObject._OnTriggerEnter)));
+        }
+
+
+
+        if (editObject.ToggleOnTriggerExit)
+        {
+            //EditorGUILayout.PropertyField(editObject);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(editObject._OnTriggerExit)));
+        }
+
+
+        if (editObject.ToggleOnTriggerStay)
+        {
+            //EditorGUILayout.PropertyField(editObject);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(editObject._OnTriggerStay)));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
