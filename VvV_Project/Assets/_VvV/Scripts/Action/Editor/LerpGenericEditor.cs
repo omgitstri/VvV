a7f0fda268d53ab0a8d08c0910c08c426784b256
trinplaytika <minhn@playtika.com>
2020-05-23 16:01:48 -0400

    feedback for charging

---
+++ VvV_Project/Assets/_VvV/Scripts/Action/Editor/LerpGenericEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LerpGeneric))]
+public class LerpGenericEditor : Editor
+{
+    LerpGeneric editObject = null;
+
+    private void OnEnable()
+    {
+        editObject = (LerpGeneric)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        switch (editObject.lerpType)
+        {
+            case LerpGeneric.EVariables.Float:
+                editObject.lerpFloatA = EditorGUILayout.FloatField("From", editObject.lerpFloatA);
+                editObject.lerpFloatB = EditorGUILayout.FloatField("To", editObject.lerpFloatB);
+                break;
+            case LerpGeneric.EVariables.Int:
+                editObject.lerpIntA = EditorGUILayout.IntField("From", editObject.lerpIntA);
+                editObject.lerpIntB = EditorGUILayout.IntField("To", editObject.lerpIntB);
+                break;
+            case LerpGeneric.EVariables.vector2:
+                editObject.lerpVector2A = EditorGUILayout.Vector2Field("From", editObject.lerpVector2A);
+                editObject.lerpVector2B = EditorGUILayout.Vector2Field("To", editObject.lerpVector2B);
+                break;
+            case LerpGeneric.EVariables.vector3:
+                editObject.lerpVector3A = EditorGUILayout.Vector3Field("From", editObject.lerpVector3A);
+                editObject.lerpVector3B = EditorGUILayout.Vector3Field("To", editObject.lerpVector3B);
+                break;
+            case LerpGeneric.EVariables.color:
+                editObject.lerpColorA= EditorGUILayout.ColorField("From", editObject.lerpColorA);
+                editObject.lerpColorB = EditorGUILayout.ColorField("To", editObject.lerpColorB);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ VvV_Project/Assets/_VvV/Scripts/Action/LerpGeneric.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpGeneric : MonoBehaviour
+{
+    public enum EVariables { Float, Int, vector2, vector3, color };
+
+    public EVariables lerpType = EVariables.vector2;
+    [SerializeField] float lerpDuration = 3f;
+
+    [HideInInspector] public float lerpFloatA;
+    [HideInInspector] public float lerpFloatB;
+
+    [HideInInspector] public int lerpIntA;
+    [HideInInspector] public int lerpIntB;
+
+    [HideInInspector] public Vector2 lerpVector2A;
+    [HideInInspector] public Vector2 lerpVector2B;
+
+    [HideInInspector] public Vector3 lerpVector3A;
+    [HideInInspector] public Vector3 lerpVector3B;
+
+    [HideInInspector] public Color lerpColorA;
+    [HideInInspector] public Color lerpColorB;
+
+    public float a = 0f;
+    private MeshRenderer meshrender = null;
+
+    void Start()
+    {
+        meshrender = GetComponent<MeshRenderer>();
+    }
+
+    [ContextMenu(nameof(StartLerp))]
+    public void StartLerp(float _lerpDuration)
+    {
+        lerpDuration = _lerpDuration;
+        StartCoroutine(nameof(LerpVector2));
+    }
+
+
+    private IEnumerator LerpVector2()
+    {
+        while (a < lerpDuration)
+        {
+            a += Time.deltaTime / lerpDuration;
+            meshrender.material.SetTextureOffset("_BaseMap", Vector2.Lerp(lerpVector2A, lerpVector2B, a));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    
+}
