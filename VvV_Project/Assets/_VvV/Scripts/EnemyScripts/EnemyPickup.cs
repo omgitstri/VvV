13c073c716cf29e07a0ec3ccd2b914a92be8acc1
trinplaytika <minhn@playtika.com>
2020-05-24 20:52:04 -0400

    expose unity event for pickup script

--- VvV_Project/Assets/_VvV/Scripts/EnemyScripts/EnemyPickup.cs
+++ VvV_Project/Assets/_VvV/Scripts/EnemyScripts/EnemyPickup.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyPickup : MonoBehaviour
 {
+    public UnityEvent pickUpEvent = null;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
@@ -11,6 +14,8 @@ public class EnemyPickup : MonoBehaviour
         {
             Entity_Tracker.Instance.collectedPart = true;
             gameObject.SetActive(false);
+
+            pickUpEvent.Invoke();
         }
     }
 }
